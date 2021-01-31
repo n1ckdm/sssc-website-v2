829f27058e5c03c8430dda8e2811c7e5c7fbbd3a
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-01-31 21:28:07 +0000

    think it's fixed

--- src/components/header.js
+++ src/components/header.js
@@ -5,19 +5,19 @@ import Logo from "../../assets/SSSC.svg"
 const Header = () => {
   return (
     <header className="header">
-      <nav className="md:p-3 py-2 px-3 z-10">
+      <nav className="md:p-3 py-2 px-3">
         <div className="container mx-auto flex flex-wrap items-center justify-start">
           <div className="flex-1 flex items-center justify-start self-start">
             <Link to="/" className="flex">
-              <span className="text-white pr-2 my-auto text-xl font-semibold">
+              <span className="text-white pr-2 my-2 text-xl font-semibold">
                 SSSC
               </span>
-              <img className="w-16" src={Logo} alt="SSSC Logo" />
+              <img className="h-12" src={Logo} alt="SSSC Logo" />
             </Link>
           </div>
           <ul
             data-menu
-            className="md:flex flex-wrap items-center justify-start text-white"
+            className="md:flex flex-wrap text-white"
           >
             <li className="md:px-6 py-2 px-3">
               <Link to="/join">Join</Link>
--- src/hooks/use-device-detect.js
+++ src/hooks/use-device-detect.js
@@ -4,8 +4,7 @@ export default function useDeviceDetect() {
   const [isMobile, setMobile] = React.useState(false);
 
   React.useEffect(() => {
-    const userAgent =
-      typeof window.navigator === "undefined" ? "" : navigator.userAgent;
+    const userAgent = typeof window.navigator === "undefined" ? "" : navigator.userAgent;
     const mobile = Boolean(
       userAgent.match(
         /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
