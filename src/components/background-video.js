bf71bef5cfbf4702f93e5d7974ff153e6420f088
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2021-01-31 20:06:35 +0000

    downsize bg image and fix mobile issues

--- src/components/background-video.js
+++ src/components/background-video.js
@@ -1,18 +1,32 @@
 import React from "react"
-import BGImg from "../../assets/sc_bg.jpg"
+import BGImg from "../../assets/sc_bg.webp"
 import BGVideoMp4 from "../../assets/sc_bg.mp4"
 import BGVideoWbm from "../../assets/sc_bg.webm"
+import useDeviceDetect from "../hooks/use-device-detect"
 
 const BackgroundVideo = () => {
+  const { isMobile } = useDeviceDetect()
+  if (isMobile) {
     return (
-        <div className="absolute inset-0 z-negative" >
-            <video className="w-full h-full object-cover hidden md:block" autoPlay="autoplay" loop="loop" muted>
-                <source type="video/mp4" src={BGVideoMp4} />
-                <source type="video/webm" src={BGVideoWbm} />
-            </video>
-            <img className="w-full object-cover h-full md:hidden" src={BGImg} />
-        </div>
+      <div className="absolute inset-0 z-negative">
+        <img className="w-full object-cover h-full md:hidden" src={BGImg} />
+      </div>
     )
+  } else {
+    return (
+      <div className="absolute inset-0 z-negative">
+        <video
+          className="w-full h-full object-cover"
+          autoPlay="autoplay"
+          loop="loop"
+          muted
+        >
+          <source type="video/mp4" src={BGVideoMp4} />
+          <source type="video/webm" src={BGVideoWbm} />
+        </video>
+      </div>
+    )
+  }
 }
 
 export default BackgroundVideo
