eff5466a1df1f440a8ce5ff3da40c45d3237e7af
Nick <drnicholasdmartin@gmail.com>
2022-09-08 21:27:52 +0100

    Update streaker-nationals-2022.js

--- src/pages/events/streaker-nationals-2022.js
+++ src/pages/events/streaker-nationals-2022.js
@@ -30,7 +30,7 @@ const StreakerNationals = ({ data }) => {
       <div className="container mx-auto text-center">
         <p className="pageTitle">2022 Streaker Nationals</p>
         <p className="text-gray-700 font-light italic mb-8">
-          South Shields Sailing Club are hosting the 2022 Streaker Nationals
+          South Shields Sailing Club are hosting the Streaker Nationals on 23rd-25 September 2022.
         </p>
         <p className="font-semibold text-lg text-justify mt-6">
           Latest Updates
@@ -39,27 +39,24 @@ const StreakerNationals = ({ data }) => {
         {/* <img className="w-11/12 md:w-2/5 mx-auto" src={TitleImg} /> */}
         <p className="font-semibold text-justify text-lg mt-6">General Info</p>
         <p className="text-gray-900 text-justify">
-          THIS TEXT NEEDS UPDATING. For more information on these aspects of the
-          event, please refer to the&nbsp;
-          <a
-            className="text-blue-700"
-            href="https://www.streaker-class.org.uk/index.asp"
-          >
-            class association website
-          </a>
-          .
+          For more information about the event, please refer to the notice of race above.
         </p>
         <img className="w-11/12 md:w-2/5 mx-auto" src={MainImg} />
         <p className="font-semibold text-lg text-justify">Race Programme</p>
         <p className="text-gray-900 text-justify">
-          Registration: Sun 22nd Aug 16.00-19.00, Mon 23rd Aug 09.00-10.30
+          Registration: Thurs 22nd September 1600 to 1900 and Fri 23rd September 0900 to 1030.
         </p>
         <p className="text-gray-900 text-justify">
-          Briefing: Mon 23rd Aug 10:30
+          Briefing: 1130 on Fri 23rd September.
         </p>
         <p className="text-gray-900 text-justify">
-          Racing: 2 races per day, 13.00 start Mon 23rd Aug, 12.00 Tue 24th-Fri
-          27th
+          Races 1 & 2: Fri 23rd Sept
+        </p>
+        <p className="text-gray-900 text-justify">
+          Races 3, 4 & 5: Sat 24th Sept
+        </p>
+        <p className="text-gray-900 text-justify">
+          Races 6 & 7: Sun 25th Sept
         </p>
       </div>
     </Layout>
